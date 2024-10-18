39aa31b5475c3a22a94f922d41bce1901ac666cc
Rayan <rayanngi20@gmail.com>
2024-10-18 15:54:45 +0200

    FIN DU JEU V1

--- Assets/scripe/PlayerHealth.cs
+++ Assets/scripe/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class PlayerHealth : MonoBehaviour
@@ -7,6 +8,31 @@ public class PlayerHealth : MonoBehaviour
     public int health = 3; 
     public string debut;
 
+    public Image Coeur1;
+    public Image Coeur2;
+    public Image Coeur3;
+
+    void Update()
+    {
+        if(health == 3)
+        {
+            Coeur1.enabled = true;
+            Coeur2.enabled = true;
+            Coeur3.enabled = true;
+        }
+        if (health == 2)
+        {
+            Coeur1.enabled = true;
+            Coeur2.enabled = true;
+            Coeur3.enabled = false;
+        }
+        if (health == 1)
+        {
+            Coeur1.enabled = true;
+            Coeur2.enabled = false;
+            Coeur3.enabled = false;
+        }
+    }
 
     public void TakeDamage(int damage)
     {
